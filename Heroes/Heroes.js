22c39ea273d4859b728d45c4364b2ee993713b98
Bruno Rezende Laranjeira <brlaranjeira@gmail.com>
2018-05-28 18:01:06 -0300

    comeca selecao de herois

---
+++ Heroes/Heroes.js
@@ -0,0 +1,19 @@
+export default class Heroes {
+    constructor() {
+        this.resources = {
+            heroes: {
+                batmanLogo: require('../img/batman-logo.png'),
+                batmanBody: require('../img/batman-body.png'),
+
+                supermanLogo: require('../img/superman-logo.png'),
+                supermanBody: require('../img/superman-body.png'),
+
+                wonderwomanLogo: require('../img/wonderwoman-logo.png'),
+                wonderwomanBody: require('../img/wonderwoman-body.png'),
+
+                flashLogo: require('../img/flash-logo.png'),
+                flashBody: require('../img/flash-body.png')
+            }, interroga: require('../img/interroga.png')
+        }
+    }
+}
--- screens/HeroSelectScreen/HeroSelectScreen.js
+++ screens/HeroSelectScreen/HeroSelectScreen.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Image, FlatList, ActivityIndicator, Text, View  } from 'react-native';
+import { TouchableWithoutFeedback, Image, FlatList, Alert, Text, View  } from 'react-native';
+import Heroes from '../../Heroes/Heroes';
 
 export default class HeroSelectScreen extends React.Component{
+
+
 
     constructor(props) {
         super(props);
         this.state = {}
+        heroesResources = new Heroes();
     }
 
     render() {
@@ -14,24 +18,31 @@ export default class HeroSelectScreen extends React.Component{
             return {
                 key: item.toUpperCase(),
                 name: item.toUpperCase(),
-                imgBody: item + '-body.png',
-                imgLogo: item + '-logo.png'
+                imgBody: heroesResources.resources.heroes[item+'Body'],
+                imgLogo: heroesResources.resources.heroes[item+'Logo'],
             };
         });
         if (Object.keys(this.props.heroSelected).length > 0) {
-            picSelected = {uri: '../../img/'+this.props.heroSelected.imgBody};
-            //picSelected = require('../../img/interroga.png');
+            picSelected = this.props.heroSelected.imgBody;
         } else {
-            picSelected = require('../../img/interroga.png');
+            picSelected = heroesResources.resources.interroga;
+
         }
+        selectFunction = this.props.onHeroChange;
         return <View style={{flex:1}}>
-            <Image style={{flex:3}} resizeMode="contain" source={picSelected}/>
+            <Image style={{flex:3}} resizeMode="cover" source={picSelected}/>
             <FlatList
+                style={{flex:1}}
                 data={heroes}
                 horizontal={true}
-                renderItem={( item ) => <Image resizeMode="contain" source={{uri:'../../'+item.imgLogo}}/>}
+                renderItem={( item ) =>(<TouchableWithoutFeedback onPress={ () => this.props.onHeroChange(item.item) }>
+                        <View>
+                            <Image  style={{flex:1,width:100}} resizeMode="stretch" source={item.item.imgLogo} />
+                        </View>
+                    </TouchableWithoutFeedback>)}
             />
+            <Text>A{this.props.heroSelected.name}</Text>
         </View>;
     }
 
-}
+}//ugr pi empenho qtde
