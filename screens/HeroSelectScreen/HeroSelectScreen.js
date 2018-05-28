a178ec9e2bc2bf7ad5bc69fc4923559084e9f696
Bruno Rezende Laranjeira <brlaranjeira@gmail.com>
2018-05-28 00:30:34 -0300

    inicia tela de selecao

--- screens/HeroSelectScreen/HeroSelectScreen.js
+++ screens/HeroSelectScreen/HeroSelectScreen.js
@@ -5,14 +5,32 @@ export default class HeroSelectScreen extends React.Component{
 
     constructor(props) {
         super(props);
-
+        this.state = {}
     }
 
     render() {
-
-        picSelected = require('../../img/interroga.png');
+        herolist=['batman','superman','wonderwoman','flash'];
+        heroes = herolist.map((item) => {
+            return {
+                key: item.toUpperCase(),
+                name: item.toUpperCase(),
+                imgBody: item + '-body.png',
+                imgLogo: item + '-logo.png'
+            };
+        });
+        if (Object.keys(this.props.heroSelected).length > 0) {
+            picSelected = {uri: '../../img/'+this.props.heroSelected.imgBody};
+            //picSelected = require('../../img/interroga.png');
+        } else {
+            picSelected = require('../../img/interroga.png');
+        }
         return <View style={{flex:1}}>
-            <Image resizeMode="contain" source={picSelected}/>
+            <Image style={{flex:3}} resizeMode="contain" source={picSelected}/>
+            <FlatList
+                data={heroes}
+                horizontal={true}
+                renderItem={( item ) => <Image resizeMode="contain" source={{uri:'../../'+item.imgLogo}}/>}
+            />
         </View>;
     }
 
